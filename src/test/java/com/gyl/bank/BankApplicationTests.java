023750c1a8cfb3eb87a5482e83246a0ef764638d
Agustin-Guarmes <agustinguarmes@gmail.com>
2023-07-31 08:32:09 -0300

    Creacion del proyecto

---
+++ src/test/java/com/gyl/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyl.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
